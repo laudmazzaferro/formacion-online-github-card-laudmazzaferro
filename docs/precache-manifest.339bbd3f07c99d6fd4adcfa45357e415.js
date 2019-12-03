11408ef09941360a1a797401861262964826f77d
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-12-03 19:12:02 +0100

    run buil

---
+++ docs/precache-manifest.339bbd3f07c99d6fd4adcfa45357e415.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5c3aedfeff297593c1de3d3c3dc543",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed0c6be3c8c4420f7f3f",
+    "url": "./static/css/main.ec0f7c08.chunk.css"
+  },
+  {
+    "revision": "2e0cf1c7007a27f3a7e6",
+    "url": "./static/js/2.a2bb9c08.chunk.js"
+  },
+  {
+    "revision": "ed0c6be3c8c4420f7f3f",
+    "url": "./static/js/main.82536809.chunk.js"
+  },
+  {
+    "revision": "8d21c8d5509f6ebac4b8",
+    "url": "./static/js/runtime-main.0cc40c07.js"
+  }
+]);
