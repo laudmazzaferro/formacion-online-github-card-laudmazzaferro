ce970bff6ca1af7fd65870ab516fa9b4ae697b2b
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-12-03 19:02:09 +0100

    add more style to card and select

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -9,28 +9,30 @@ const Card = props => {
   const { member , selectMember , months} = props;
     if(selectMember !== ''){
       return (
+        <div className="info__member">
         <div className="card__container">
           <div className="card__user">
             <img src={`${member.avatar_url}`} alt={`Imagen de ${member.login}`} className="card__img"/>
             <p className="card__login">@{member.login}</p>
-            <h2 className="card__name">{member.name}</h2>
-            <p className="card__location"><i class="fas fa-map-marker-alt"></i> {member.location}</p>
+            {(member.name !== null) ? <h2 className="card__name">{member.name}</h2> : ''}
+            {(member.location !== null) ? <p className="card__location"><i class="fas fa-map-marker-alt"></i> {member.location}</p>:''}
           </div>
           <div className="card__info">
-            <div className="card__repos">
-              <p className="card__repos--num">{member.public_repos}</p>
-              <p className="card__repos--text">Repos</p>
+            <div className="card__info--item">
+              <p className="card__info--num">{member.public_repos}</p>
+              <p className="card__info--text">Repos</p>
             </div>
-            <div className="card__followers">
-              <p className="card__followers--num">{member.followers}</p>
-              <p className="card__followers--text">Followers</p>
+            <div className="card__info--item">
+              <p className="card__info--num">{member.followers}</p>
+              <p className="card__info--text">Followers</p>
             </div>
-            <div className="card__following">
-              <p className="card__following--num">{member.following}</p>
-              <p className="card__following--text">Following</p>
+            <div className="card__info--item">
+              <p className="card__info--num">{member.following}</p>
+              <p className="card__info--text">Following</p>
             </div>
           </div>
-          <p className="card__months">Miembro desde hace {months} meses</p>
+        </div>
+        <p className="card__months">Miembro desde hace {months} meses</p>
         </div>
       )
     }else{
