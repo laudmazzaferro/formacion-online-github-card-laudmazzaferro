a9f94f6fba0ad57390567a35b1865038305a161a
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-12-03 12:27:15 +0100

    create components and imports on app

---
+++ src/Components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+//import ContextMenu from 'react-context-menu';
+
+
+const Card = props => {
+    return (
+      <div>
+        card
+      </div>
+    )}
+
+Card.propTypes = {
+  
+};
+      
+export default Card;
---
+++ src/Components/Select.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+//import ContextMenu from 'react-context-menu';
+
+
+const Select = props => {
+    return (
+      <div>
+        select
+      </div>
+    )}
+
+Select.propTypes = {
+  
+};
+      
+export default Select;
