d4c0f5299380da0037b45da318ecd2ede2f664a0
laura diaz <laura.diaz.mazzaferro@gmail.com>
2019-12-03 13:56:15 +0100

    Create the getMember

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Select from './Components/Select';
 import Card from './Components/Card';
 import { fetchMembers } from './services/fetchMembers';
+import { fetchMember }  from '';
 import './App.css';
 
 class App extends React.Component {
@@ -9,26 +10,42 @@ class App extends React.Component {
     super(props);
     this.state = {
       members: [],
+      selectMember: '',
+      member:{}
     }
     this.getMembers = this.getMembers.bind(this);
+    this.getSelectMember = this.getSelectMember.bind(this);
+    this.getMemberInfo = this.getMemberInfo.bind(this);
   }
   componentDidMount() {
     this.getMembers();
-  }
-  getMembers(){
+  };
+  getMembers() {
     fetchMembers()
-    .then(data=>{
-      this.setState({
-        members:data
-      },()=>{console.log(this.state.members)})
-    })
+      .then(data => {
+        this.setState({
+          members: data
+        }, () => { console.log(this.state.members) })
+      });
+  };
+  getSelectMember(value) {
+    this.setState({
+      selectMember: value
+    },()=>{this.getMemberInfo()});
+  };
+
+  getMemberInfo(){
+
   }
 
+
+
   render() {
+    const { members } = this.state;
     return (
       <div className="App">
         <main>
-          <Select></Select>
+          <Select members={members} getSelectMember={this.getSelectMember}></Select>
           <Card></Card>
         </main>
 
--- src/Components/Select.js
+++ src/Components/Select.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-//import ContextMenu from 'react-context-menu';
 
 
-const Select = props => {
+class Select extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getSelectValue = this.getSelectValue.bind(this);
+  }
+  getSelectValue(event){
+    const {value}= event.currentTarget;
+    this.props.getSelectMember(value);
+  }
+
+  render() {
+    const { members } = this.props;
     return (
-      <div>
-        select
+      <div className="select__members--container">
+        <label htmlFor="members" className="select__members--label"></label>
+        <select id="members" name="members" placeholder="Selecciona un usuario" className="select__members" onChange={this.getSelectValue} >
+          {members.map(member => {
+            return (<option value={member.login} key={member.id}className="select__members--option">{member.login}</option>)
+          })}
+        </select>
       </div>
-    )}
+    )
+  }
+
+}
 
 Select.propTypes = {
-  
+
 };
-      
+
 export default Select;
